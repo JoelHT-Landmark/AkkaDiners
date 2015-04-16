23af1cb6d195de7e90f7531e2f0b8156b62e213a
Rammesses <joel@cheviot-consulting.co.uk>
2015-04-16 12:01:33 +0100

    Added Serilog package and basic configuration for console and rolling flat file logging.

--- Diners/App.xaml.cs
+++ Diners/App.xaml.cs
@@ -1,6 +1,7 @@
 ﻿namespace Diners
 {
     using Metrics;
+    using Serilog;
     using System.Windows;
 
     /// <summary>
@@ -11,6 +12,11 @@
         public App()
         {
             Metric.Config.WithHttpEndpoint("http://localhost:1234/");
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Verbose()
+                .WriteTo.ColoredConsole()
+                .WriteTo.RollingFile("Diners-{Date}.txt")
+                .CreateLogger();
         }
     }
 }
