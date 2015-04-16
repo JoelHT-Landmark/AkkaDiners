60e62c48300c4b61ae1592752826583e75e49d07
Rammesses <joel@cheviot-consulting.co.uk>
2015-04-16 13:18:30 +0100

    Implemented toolbar with dynamic logging level control.

--- Diners/App.xaml.cs
+++ Diners/App.xaml.cs
@@ -2,6 +2,8 @@
 {
     using Metrics;
     using Serilog;
+    using Serilog.Core;
+    using Serilog.Events;
     using System.Windows;
 
     /// <summary>
@@ -9,14 +11,21 @@
     /// </summary>
     public partial class App : Application
     {
+        private readonly LoggingLevelSwitch logLevelSwitch = new LoggingLevelSwitch();
+
         public App()
         {
             Metric.Config.WithHttpEndpoint("http://localhost:1234/");
             Log.Logger = new LoggerConfiguration()
-                .MinimumLevel.Verbose()
+                .MinimumLevel.ControlledBy(this.logLevelSwitch)
                 .WriteTo.ColoredConsole()
                 .WriteTo.RollingFile("Diners-{Date}.txt")
                 .CreateLogger();
+        }
+
+        public void SetLoggingLevel(LogEventLevel level)
+        {
+            this.logLevelSwitch.MinimumLevel = level;
         }
     }
 }
--- Diners/MainWindow.xaml.cs
+++ Diners/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿namespace Diners
 {
+    using Serilog.Events;
     using System;
     using System.Windows;
 
@@ -12,14 +13,27 @@
 
         private DinersSystem system;
 
+        private readonly App app = App.Current as App;
+
         public MainWindow()
         {
             InitializeComponent();
 
             this.StartButton.Click += StartDining;
+            this.LoggingLevelSlider.ValueChanged += LoggingLevelChanged;
 
             outputter = new TextBoxOutputter(TestBox);
             Console.SetOut(outputter);
+        }
+
+        private void LoggingLevelChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            var levelValue = Convert.ToInt32(e.NewValue);
+            var level = (LogEventLevel) Enum.Parse(typeof(LogEventLevel), levelValue.ToString());
+
+            app.SetLoggingLevel(level);
+
+            this.LevelName.Content = level.ToString();
         }
 
         private void StartDining(object sender, RoutedEventArgs e)
@@ -27,5 +41,7 @@
             this.system = new DinersSystem();
             this.system.StartDining();
         }
+
+
     }
 }
